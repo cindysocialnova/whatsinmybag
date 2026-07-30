a65550c31b95e414f0a7041839bcca28723ba261
Cindy R <cindysocialnova@gmail.com>
2026-07-30 08:28:11 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ const splitData = [
     image: 'images/email mockup.png',
     alt: 'Beauty Statute Product',
     title: 'About Me!',
-    description: 'Marketing professional with experience in social media marketing, analytics, and search optimization. I love combine data and branding to create a unipe and dythimic marketing ca[...]',
+    description: 'Marketing professional with experience in social media marketing, analytics, and search engine optimization. I love combining data and branding to create unique and dynamic marketing campaigns.'
     buttonText: 'See My Work',
     buttonLink: '#work-four-target'
   },
@@ -32,7 +32,7 @@ const splitData = [
     image: 'images/email mockup1.png',
     alt: 'Product Image',
     title: 'Services',
-    description: 'I bridge the gap between creative strategy and data analytics to build high-converting content pipelines, ensuring compelling visual storytelling and a cohesive brand presence ac[...]',
+    description: 'I bridge the gap between creative strategy and data analytics to build high-converting content pipelines, ensuring compelling visual storytelling and a cohesive brand presence.'
     buttonText: 'Book Me',
     buttonLink: 'https://cindyrmarketing.setmore.com/cindyr'
   }
